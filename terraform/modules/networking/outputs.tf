6ff9a6ede3b807bc04a1fab8c8212cc93ffa50fe
sudipto <sudiptorocks@proton.me>
2025-03-22 18:42:35 +0600

    networking module outputs added

---
+++ terraform/modules/networking/outputs.tf
@@ -0,0 +1,11 @@
+output "main_vpc" {
+  value = aws_vpc.main-vpc
+}
+
+output "public_subnet_id" {
+  value = aws_subnet.public_subnet.id
+}
+
+output "private_subnet_id" {
+  value = aws_subnet.private_subnet.id
+}

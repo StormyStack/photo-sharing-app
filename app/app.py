d487e9965c0139a8d326cab6f29ae86c27d25bde
sudipto <sudiptorocks@proton.me>
2025-03-27 04:20:50 +0600

    in app DynamoDB functions added

--- app/app.py
+++ app/app.py
@@ -1,12 +1,27 @@
-from flask import Flask, render_template, redirect, request, jsonify
+from flask import Flask, render_template, request, jsonify
 import boto3
 from werkzeug.utils import secure_filename
 import os
+import uuid
+from datetime import datetime
 
-app=Flask(__name__)
-app.config.from_object('config.Config')
+app = Flask(__name__)
+app.config.from_object("config.Config")
 
-s3 = boto3.client('s3', aws_access_key_id=app.config['AWS_ACCESS_KEY'], aws_secret_access_key=app.config['AWS_SECRET_KEY'])
+S3_BUCKET_NAME = app.config["S3_BUCKET_NAME"]
+DYNAMODB_TABLE_NAME = app.config["DYNAMODB_TABLE_NAME"]
+
+s3 = boto3.client(
+    "s3",
+    aws_access_key_id=app.config["AWS_ACCESS_KEY"],
+    aws_secret_access_key=app.config["AWS_SECRET_KEY"]
+)
+dynamodb = boto3.resource(
+    "dynamodb",
+    aws_access_key_id=app.config["AWS_ACCESS_KEY"],
+    aws_secret_access_key=app.config["AWS_SECRET_KEY"]
+)
+table = dynamodb.Table(DYNAMODB_TABLE_NAME)
 
 @app.route("/")
 def index():
@@ -16,14 +31,30 @@ def index():
 def upload_file():
     file = request.files["file"]
     if file:
-        s3.upload_fileobj(file, S3_BUCKET_NAME, file.filename)
+        filename = secure_filename(file.filename)
+        photo_id = str(uuid.uuid4())
+        s3_key = f"{photo_id}_{filename}"
+        photo_url = f"https://{S3_BUCKET_NAME}.s3.amazonaws.com/{s3_key}"
+
+        s3.upload_fileobj(file, S3_BUCKET_NAME, s3_key, ExtraArgs={"ACL": "public-read"})
+
+        table.put_item(
+            Item={
+                "photo_id": photo_id,
+                "photo_url": photo_url,
+                "uploaded_at": datetime.utcnow().isoformat(),
+            }
+        )
+
         return "File uploaded successfully!", 200
     return "No file uploaded!", 400
 
 @app.route("/images")
 def list_images():
-    objects = s3.list_objects_v2(Bucket=S3_BUCKET_NAME).get("Contents", [])
-    image_urls = [f"https://{S3_BUCKET_NAME}.s3.amazonaws.com/{obj['Key']}" for obj in objects]
+    response = table.scan()
+    items = response.get("Items", [])
+    
+    image_urls = [{"photo_url": item["photo_url"], "uploaded_at": item["uploaded_at"]} for item in items]
     return jsonify(image_urls)
 
 if __name__ == "__main__":
--- app/config.py
+++ app/config.py
@@ -3,5 +3,5 @@ class Config:
     AWS_ACCESS_KEY=os.getenv('AWS_ACCESS_KEY_ID')
     AWS_SECRET_KEY=os.getenv('AWS_SECRET_ACCESS_KEY')
     S3_BUCKET_NAME=os.getenv('S3_BUCKET_NAME')
+    DYNAMODB_TABLE_NAME = os.getenv("DYNAMODB_TABLE_NAME")
     FLASK_ENV='development'
-    

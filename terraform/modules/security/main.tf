b0e6ce5df5f2070b0b3826240fca4365c7055356
sudipto <sudiptorocks@proton.me>
2025-03-28 23:54:20 +0600

    Fixed some problem

--- terraform/modules/security/main.tf
+++ terraform/modules/security/main.tf
@@ -9,7 +9,7 @@ resource "aws_security_group" "alb_sg" {
   egress {
     from_port = 80
     to_port = 80
-    protocol = "tpc"
+    protocol = "tcp"
     security_groups = [aws_security_group.ec2_sg.id]
   }
   tags = {
@@ -34,7 +34,7 @@ resource "aws_security_group" "ec2_sg" {
   egress {
     from_port = 443
     to_port = 443
-    protocol = "tpc"
+    protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
   tags = {

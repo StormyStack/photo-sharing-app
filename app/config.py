2fa92e4530b9269dd378f2ecc9e1d2f8d5513cb7
sudipto <sudiptorocks@proton.me>
2025-04-02 00:54:24 +0600

    Hardcoded env variable in config file

--- app/config.py
+++ app/config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    S3_BUCKET_NAME = os.getenv("S3_BUCKET_NAME", "default-s3-bucket-name")
-    DYNAMODB_TABLE_NAME = os.getenv("DYNAMODB_TABLE_NAME", "default-dynamodb-table-name")
-    AWS_REGION = os.getenv("AWS_REGION", "default-aws-region")
+    S3_BUCKET_NAME = "bokkar70-photo"
+    DYNAMODB_TABLE_NAME = "photo_metadata"
+    AWS_REGION = "us-east-1"

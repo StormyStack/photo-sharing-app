280c9ed97e165c6ee806c75084d9386d11e701f5
sudipto <sudiptorocks@proton.me>
2025-04-08 10:53:40 +0600

    use s3 bucket name as env variable

--- app/config.py
+++ app/config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    S3_BUCKET_NAME = "sifat-69-photo"
+    S3_BUCKET_NAME = os.getenv('s3_bucket_name')
     DYNAMODB_TABLE_NAME = "photo_metadata"
     AWS_REGION = "us-east-1"
